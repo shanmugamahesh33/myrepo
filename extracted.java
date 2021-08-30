8c9b81534a543f45eec610342a56b261c240a901
shanmugamahesh33 <89769976+shanmugamahesh33@users.noreply.github.com>
2021-08-30 13:29:42 +0530

    Add files via upload

---
+++ extracted.java
@@ -0,0 +1,28 @@
+import java.io.*;
+class Assignment{
+    public static void main(String[] args){
+        String file="C:\\Users\\SHANMUGA MAHESH\\Desktop\\main.csv";
+        BufferedReader reader=null;
+        String line="";
+
+        try{
+            reader=new BufferedReader(new FileReader(file));
+            while((line= reader.readLine()) !=null){
+                String[] row=line.split(",");
+
+                for(String index:row){
+                    System.out.printf("%10s",index);
+                }
+                System.out.println();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                reader.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
